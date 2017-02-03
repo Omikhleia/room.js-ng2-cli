b93abaee1662920d3e4ccaa11ae20310bcd04c06
Omikhleia <didier.willis@gmail.com>
2017-02-03 20:33:31 +0100

    Add @ prefix override for playmode

--- demo/world/system/preprocessCommand.js
+++ demo/world/system/preprocessCommand.js
@@ -1,3 +1,12 @@
 function preprocessCommand(player, input) {
+  const prefix = input.substring(0, 1);
+  if (prefix === '@') {
+    // Consider inputs starting with @ as direct play command
+    // (whatever mode is active)
+    const direct = input.substring(1);
+    return modes_playmode.preprocessCommand(direct);
+  }
+
   return player.mode.preprocessCommand(input);
 }
+
