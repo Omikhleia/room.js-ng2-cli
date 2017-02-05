050058aab3f12a800a88a656396808d825851ae4
Omikhleia <didier.willis@gmail.com>
2017-02-05 14:30:28 +0100

    Add: Map - Basic isometric view

--- roomjs-client2/src/app/mappane/mappane.component.ts
+++ roomjs-client2/src/app/mappane/mappane.component.ts
@@ -10,6 +10,45 @@ export class MappaneComponent implements OnInit {
   @Input() exits: string[];
   @Output() commandEntered = new EventEmitter<string>();
 
+  private blocks: any = [
+    {x: 200, y: 0, dir: 'northwest' },
+    {x: 300, y: 50, dir: 'north' },
+    {x: 400, y: 100, dir: 'northeast' },
+    {x: 100, y: 50, dir: 'west' },
+    {x: 200, y: 100, dir: 'up' },
+    {x: 200, y: 100, dir: 'down' },
+    {x: 300, y: 150, dir: 'east' },
+    {x: 0, y: 100, dir: 'southwest' },
+    {x: 100, y: 150, dir: 'south' },
+    {x: 200, y: 200, dir: 'southeast' }
+  ];
+  
+  private exitBlocks: any = {
+    northwest: "wd.png",
+    north: "nd.png",
+    northeast: "nd.png",
+    west: "wd.png",
+    up: "ud.png",
+    down: "dd.png",
+    east: "ed.png",
+    southwest: "sd.png",
+    south: "sd.png",
+    southeast: "sd.png"
+  };
+
+ private doors: any = [  
+    {x: 150, y: 125, dir: 'northwest' },
+    {x: 350, y: 175, dir: 'north' },
+    {x: 450, y: 225, dir: 'northeast' },
+    {x: 50, y: 175, dir: 'west' },
+    {x: 175, y: 237.5, dir: 'up' },
+    {x: 220, y: 275, dir: 'down' },
+    {x: 350, y: 325, dir: 'east' },
+    {x: -50, y: 275, dir: 'southwest' },
+    {x: 50, y: 325, dir: 'south' },
+    {x: 250, y: 375, dir: 'southeast' }
+ ];
+    
   // Constructor  
   constructor() { }
 
@@ -30,7 +69,7 @@ export class MappaneComponent implements OnInit {
       // SVG element with parent link (e.g. surrounded with a SVG <a>
       hash = event.target.parentElement.getAttribute('xlink:href');
     }
-      
+    
     const pattern = /#cmd\[(.*?)\]/g;
     const match = pattern.exec(hash);
     if (!match) { 
@@ -55,6 +94,15 @@ export class MappaneComponent implements OnInit {
       return true;
     }
     return false;
+  }
+  
+  private dirImage(dir: string): string {
+    const prefix = './assets/images/';
+    return prefix + this.exitBlocks[dir];
+  }
+  
+  private dirCommand(dir: string): string {
+    return `#cmd[${dir}]`;
   }
 
 }
