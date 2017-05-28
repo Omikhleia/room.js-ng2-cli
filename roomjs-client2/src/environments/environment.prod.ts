b4ae8d329a2915e77452171078473b9249a00088
Omikhleia <didier.willis@gmail.com>
2017-05-28 15:43:01 +0200

    Fix: Back to default port for game engine

--- roomjs-client2/src/environments/environment.prod.ts
+++ roomjs-client2/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverUrl: 'http://localhost:9999'
+  serverUrl: 'http://localhost:8888'
 };
--- roomjs-client2/src/environments/environment.ts
+++ roomjs-client2/src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  serverUrl: 'http://localhost:9999'
+  serverUrl: 'http://localhost:8888'
 };
